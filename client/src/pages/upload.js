436783dbe253cfbcfa66a0bcea8edd60fbcf6d43
Phernoix <eklaseboer@gmail.com>
2022-02-16 15:23:04 +0800

    changed ui element

--- client/src/pages/upload.js
+++ client/src/pages/upload.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import FileUpload from '../components/fileUpload.js';
+import Footer from '../components/footer.js';
 
 const Upload = () => {
     return(
+    <div>
     <div className='container mt-4'>
     <h4 className='display-4 text-center mb-4'>
       <i className='fab fa-react' /> React File Upload
     </h4>
 
     <FileUpload />
+  </div>
+  <Footer/>
   </div>
   );
 

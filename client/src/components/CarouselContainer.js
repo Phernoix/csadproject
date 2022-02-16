96612e8d5bb31b0cc7ef2cb0a2c40a946641f8fb
Phernoix <eklaseboer@gmail.com>
2022-02-16 21:34:29 +0800

    changed footer size and added some text

--- client/src/components/CarouselContainer.js
+++ client/src/components/CarouselContainer.js
@@ -6,28 +6,32 @@ import Raja from "../assets/img/stat/raja.jpeg";
 import Blog1 from "./../assets/img/blog/blog1.jpg";
 import Blog2 from "./../assets/img/blog/blog2.jpg";
 import Blog3 from "./../assets/img/blog/blog3.jpg";
+import Blog4 from "./../assets/img/blog/blog4.jpg";
+import Blog5 from "./../assets/img/blog/blog5.jpg";
+import Blog6 from "./../assets/img/blog/blog6.jpg";
 import Fit1 from "./../assets/img/carusel/fit1.png";
+import "./carousel.css";
 
 const CarouselContainer = () => {
   return (
     <Carousel>
-      <Carousel.Item interval={1000}>
-        <img className="d-block w-100" src={Blog1} alt="First slide" />
+      <Carousel.Item interval={2000}>
+        <img className="d-block w-100" src={Blog4} alt="First slide" />
         <Carousel.Caption>
           <h3>Fitness is the best</h3>
           <p>You have to work out</p>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item interval={500}>
-        <img className="d-block w-100" src={Blog2} alt="Second slide" />
+      <Carousel.Item interval={2000}>
+        <img className="d-block w-100" src={Blog5} alt="Second slide" />
         <Carousel.Caption>
-          <h3>Start working out today</h3>
-          <p>Exercising helps your physical and mental stress</p>
+          <h3>Learn about sexual wellness today</h3>
+          <p>Having regular sex helps your physical and mental stress</p>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item>
-        <img className="d-block w-100" src={Blog3} alt="Third slide" />
-        <Carousel.Caption>
+      <Carousel.Item interval={2000}>
+        <img className="d-block w-100" src={Blog6} alt="Third slide" />
+        <Carousel.Caption mb-4>
           <h3>Singapore's health and welness</h3>
           <p>Health and wellness is key</p>
         </Carousel.Caption>
